9b3b5e9984002d98f5fb72896d5bc3d082ac5009
Lucas <lucasgottifredi@hotmail.com>
2024-07-16 21:07:12 -0300

    Se agregan dos mas y se deja el que ya estaba

---
+++ scripts/prueba4.sql
@@ -0,0 +1,10 @@
+USE clinica_saezco;
+
+CREATE TABLE prueba4 (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(255) NOT NULL,
+    apellido VARCHAR(255) NOT NULL,
+    edad INT NOT NULL,
+    email VARCHAR(255),
+    fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
