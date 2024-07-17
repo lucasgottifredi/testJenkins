a0c72fbddbae74ffbabe6d789afdd120e28451f3
Lucas <lucasgottifredi@hotmail.com>
2024-07-16 21:16:41 -0300

    Quinta prueba

---
+++ scripts/prueba5.sql
@@ -0,0 +1,10 @@
+USE clinica_saezco;
+
+CREATE TABLE prueba5 (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(255) NOT NULL,
+    apellido VARCHAR(255) NOT NULL,
+    edad INT NOT NULL,
+    email VARCHAR(255),
+    fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
